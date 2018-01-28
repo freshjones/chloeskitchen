1a834792a78f1babe584cc7395993cef610e65e4
William Jones <billy@freshjones.com>
2018-01-28 14:58:24 -0500

    working

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,32 +2,54 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          Gatsby
-        </Link>
-      </h1>
+  <header class="site-header jw-header" itemscope="" itemtype="https://schema.org/WPHeader">
+    <div class="wrap">
+      <div class="title-area">
+        <p class="site-title" itemprop="headline">
+          <a href="/">Chloe's Kitchen</a>
+        </p>
+      </div>
+      <nav 
+        class="nav-header" 
+        itemscope="" 
+        itemtype="https://schema.org/SiteNavigationElement"
+      >
+        <ul>
+          <li><a href="/who-i-am" itemprop="url"><span itemprop="name">About</span></a></li>
+          <li><a href="/recipes" itemprop="url"><span itemprop="name">Recipes</span></a></li>
+          <li><a href="/shop" itemprop="url"><span itemprop="name">Shop</span></a></li>
+          <li class="menu-item socials">
+            <a href="https://www.facebook.com/chloes-kitchen" class="social-facebook" target="_blank"><i class="icon-facebook"></i></a> 
+            <a href="https://www.instagram.com/chloes-kitchen" class="social-instagram" target="_blank"><i class="icon-instagram"></i></a> 
+            <a href="https://www.twitter.com/chloes-kitchen" class="social-twitter" target="_blank"><i class="icon-twitter"></i></a> 
+            <a href="https://www.pinterest.com/chloes-kitchen/" class="social-pinterest" target="_blank"><i class="icon-pinterest"></i></a> 
+            <a href="https://www.bloglovin.com/blogs/chloes-kitchen" class="social-bloglovin" target="_blank"><i class="icon-bloglovin"></i></a> 
+            <a href="https://feeds.feedburner.com/chloes-kitchen" class="social-feedly" target="_blank"><i class="icon-feedly"></i></a>
+          </li>
+          <li class="menu-item search">
+            <form 
+              class="search-form" 
+              itemprop="potentialAction" 
+              itemscope="" 
+              itemtype="https://schema.org/SearchAction" 
+              method="get" 
+              action="https://chloes.kitchen/" 
+              role="search"
+            >
+              <input value="" type="text" placeholder="Search for a Recipe" />
+              <input value="Submit" type="submit"  />
+            </form>
+          </li>
+        </ul>
+      </nav>
+      <div class="clear-line from-theme"></div>
+      <a href="#" class="mobile-menu-toggle">
+        <span class="top-bar"></span>
+        <span class="middle-bar"></span>
+        <span class="bottom-bar"></span>
+      </a>
     </div>
-  </div>
+  </header>
 )
 
 export default Header
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,19 +2,24 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
+import NewsletterHeader from '../components/NewsletterHeader'
 import Header from '../components/Header'
+import Featured from '../components/Featured'
+import {About} from '../components/Spotlights'
 import './index.css'
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Chloe's Kitchen - A food blog with simple and tasty recipes"
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: 'A food blog with simple, delicious recipes' },
       ]}
     />
+    <NewsletterHeader />
     <Header />
+    <Featured />
+    <About />
     <div
       style={{
         margin: '0 auto',
